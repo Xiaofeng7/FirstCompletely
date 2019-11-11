f47d1768335314b93163be99439e8fb723df3131
gxf <gxf@DESKTOP-83G5M9O>
2019-11-11 11:51:44 +0800

    添加文件及图片上传模块，以及博客编辑界面，利用kindeditor插件实现。

--- src/com/gkk/ssm/mapper/UserAccountMapper.java
+++ src/com/gkk/ssm/mapper/UserAccountMapper.java
@@ -7,15 +7,15 @@ import com.gkk.ssm.po.UserAccount;
 
 public interface UserAccountMapper {
 
-	List<UserAccount> findAccount(String username)throws Exception;//通过用户名查找
+	List<UserAccount> findAccount(String username)throws Exception;
 	
-	UserAccount findAccountById(Integer id)throws Exception;//通过id查找
+	UserAccount findAccountById(Integer id)throws Exception;
 	
-	Integer insertAccount(UserAccount account)throws Exception;//插入用户,返回值为插入数据库的条数
+	Integer insertAccount(UserAccount account)throws Exception;
 	
-	Integer deleteAccount(Integer id)throws Exception;//通过id删除用户,返回值为删除的数据条数
+	Integer deleteAccount(Integer id)throws Exception;
 	
-	Integer updatePassword(Instrument instrument)throws Exception;//修改密码，配置useAffectedRows=true后返回值为更新的条数
+	Integer updatePassword(Instrument instrument)throws Exception;
 
 	
 	
